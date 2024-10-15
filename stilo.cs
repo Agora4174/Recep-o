904764f61025db098bc68dd0116aa1e2c8a69a4e
agora consultoria ti <95979346+Agora4174@users.noreply.github.com>
2024-10-15 14:39:56 -0300

    Add files via upload

---
+++ stilo.cs
@@ -0,0 +1,164 @@
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: Arial, sans-serif;
+    line-height: 1.6;
+    display: flex;
+    flex-direction: column;
+    min-height: 90vh;
+    background-color:#EE82EE;
+
+
+
+    
+}
+
+header {
+    background-color: #FF00FF;
+    color: white;
+    text-align: center;
+    left: auto;
+}
+
+header img{
+width: 50px;
+    border-radius: 50px;
+    height: 50px;
+    float: left;
+}
+main {
+    padding: 10px;
+}
+
+h1, h2 {
+    text-align: center;
+}
+
+
+
+ul {
+    list-style-type: none;
+ 
+    
+}
+
+ul li a{
+  padding: 1px 10px 1px 1px;
+    color:black;
+     text-decoration: none;
+    width: 1010px;
+    border-radius: 20px;
+   align-content: center; 
+
+   
+
+        
+}
+
+
+ul li  {
+color:black;
+    background-color:#9932CC;
+    text-decoration: none;
+    font-size: 1.2rem;
+         margin: 10px 10px;
+    width: 150px;
+    border-radius: 10px;
+     text-align: center;
+    display:inline-block;
+   
+
+    
+ 
+   }
+
+
+
+
+
+ img {
+ width: 150px;
+     align-content: center;
+
+    
+
+    
+}
+
+
+footer {
+    background-color:#FF00FF;
+    color: white;
+    text-align: center;
+    padding: 10px;
+    position: relative;
+}
+
+
+
+
+
+
+
+ .container {
+            max-width: 700px;
+            margin: 100px auto;
+            padding: 200px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+
+        th, td {
+            padding: 12px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+
+
+
+
+
+@media (max-width: 600px) {
+    h1, h2 {
+        font-size: 1.5em;
+    }
+
+    ul li a {
+        font-size: 1em;
+
+        
+    }
+
+
+}
